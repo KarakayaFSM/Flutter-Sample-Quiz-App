e358dd35668d3f19485836f34aa5b73416004ef4
salih <salih.karakaya@codefirst.io>
2022-03-30 09:05:44 +0300

    add question and answer widgets

---
+++ lib/answer.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Answer extends StatelessWidget {
+  final VoidCallback selectHandler;
+  final String answerText;
+
+  const Answer(this.selectHandler, this.answerText, {Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(onPrimary: Colors.black),
+        onPressed: selectHandler,
+        child: Text(answerText),
+      ),
+    );
+  }
+}
